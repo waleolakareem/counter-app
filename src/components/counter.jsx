58a07dbdf71bd12274c842f18f1caa781f2abd28
Oladejo <wale@senyours.io>
2019-04-20 18:43:36 -0700

    How to use constructors and bind

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,19 +7,27 @@ class Counter extends Component {
   };
 
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return <ul>
-          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
+  //First way to get constructor and binding the function
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  //Second appreoach for binding is using the converting it in arrow function
+
+
+
+  handleIncrement = () => {
+    console.log("Increment Clicked",this)
   }
 
   render() {
     return <div>
-      <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-      <button className="btn btn-secondary btn-sm">Increment</button>
-      {this.state.tags.length === 0 && "Please create a new tag!"}
-      {this.renderTags()}
+    <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+    <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment
+    </button>
+    {this.state.tags.length === 0 && "Please create a new tag!"}
+    {this.renderTags()}
     </div>;
   }
 
@@ -29,6 +37,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     return this.state.count === 0? "btn m-2 badge-warning": "btn m-2 badge-primary";
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return <ul>
+    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+    </ul>
   }
 
 }
